0a6df450ebfe862c889ddfc6dda202ee23513343
Katherine Domingo <katdom13@gmail.com>
2017-09-22 00:39:50 +0800

    Add project files.

---
+++ QLearningAI/RL_Brain.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLearningAI
+{
+    class RL_Brain
+    {
+        private int[] Actions { get; set; }
+        private double Alpha { get; set; } //learning rate
+        private double Gamma { get; set;  } //reward decay
+        private double Epsilon { get; set; } //greedy attribute
+        private Dictionary<object[], double[]> Q_Table { get; set; }
+
+        
+
+        public RL_Brain(int[] actions)
+        {
+            Alpha = 0.1;
+            Gamma = 0.9;
+            Epsilon = 0.9;
+
+            Actions = actions;
+
+            Q_Table = new Dictionary<object[], double[]>();
+
+        }
+        
+        private Random rand = new Random();
+
+        public int ChooseAction(object[] speed_ball_state)
+        {
+            int A = 0;
+
+            CheckIfStateExists(speed_ball_state);
+
+            if (rand.NextDouble() < Epsilon)
+            {
+                double[] state_actions = Q_Table[speed_ball_state];
+                Shuffle(state_actions);
+
+                //max action
+                var maxAction = state_actions.Max();
+                var indexOfMax = Array.IndexOf(state_actions, maxAction);
+
+                A = Actions[indexOfMax];
+
+            }
+            else
+            {
+                A = Actions[rand.Next(Actions.Length)];
+            }
+
+            return A;
+
+        }
+
+        public void Learn(object[] S, int A, int R, object[] S_)
+        {
+
+            double q_target = 0.0;
+
+            CheckIfStateExists(S_);
+
+            double q_predict = Q_Table[S][A];
+
+            int[] terminal = new int[]{ -1, -1 };
+
+            if (S.Equals(terminal))
+            {
+                q_target = R + Gamma * Q_Table[S_].Max();
+            }
+            else
+            {
+                q_target = R;
+            }
+
+            Q_Table[S][A] += Alpha * (q_target - q_predict);
+
+        }
+
+        public void CheckIfStateExists(object[] speed_ball)
+        {
+            if (Q_Table.ContainsKey(speed_ball) == false)
+            {
+                Q_Table.Add(speed_ball, new double[Actions.Length]);
+            }
+        }
+
+        public void Shuffle<T>(IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rand.Next(n + 1);
+
+                T value = list[k];
+                int key = Actions[k];
+
+                list[k] = list[n];
+                Actions[k] = Actions[n];
+
+                list[n] = value;
+                Actions[n] = key;
+            }
+        }
+
+    }
+}
---
+++ QLearningAI/World.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLearningAI
+{
+    class World
+    {
+        private int[] ActionSpace { get; set; }
+        private int N_Actions = 0;
+
+        //from env
+        private int Speed { get; set; }
+        private int Balls { get; set; }
+        private double Playtime { get; set; }
+
+        //private int PlayerScore { get; set; }
+
+        //private int[] S, S_;
+
+        public World(int[] actions)
+        {
+            ActionSpace = actions;
+            N_Actions = ActionSpace.Length;
+        }
+
+        public int Get_N_Actions()
+        {
+            return N_Actions;
+        }
+
+        public void BuildInitialObservation()
+        {
+            //get speed from unity
+            //get balls from unity
+            //get time/score from unity
+        }
+
+        public object[] Reset()
+        {
+
+            Speed = 0;  //reset from stored/orig
+            Balls = 0;  // reset from stored/orig
+
+            int speed = Speed;
+            int balls = Balls;
+            double playtime = Playtime;
+
+            object[] state = { speed, balls, playtime };
+
+            return state;
+        }
+
+        public bool Step(int action, out object[] S_, out int reward)
+        {
+            //get player score, score must be within (20, 50) for example
+            bool done = false;
+
+            if(action == 0) //increase speed
+            {
+                Speed += 10;
+            }else if(action == 1) // decrease speed
+            {
+                Speed -= 10;
+            }else if(action == 2) // increase balls
+            {
+                Balls += 1;
+            }else if(action == 3) // decrease balls
+            {
+                Balls -= 1;
+            }
+
+            //What defines the NEXT STATE is the ball's new speed and number
+            //THIS IS THE NEXT STATE S_
+            int speed = Speed;
+            int balls = Balls;
+            double playtime = Playtime;
+
+            S_ = new object[]{ speed, balls, playtime };
+
+            if (Enumerable.Range(50, 120).Contains(Convert.ToInt32(S_[2])))
+            {
+                reward = 1;
+                done = true;
+            }
+            else
+            {
+                reward = 0;
+                done = false;
+            }
+            return done;
+        }
+
+
+
+    }
+}
